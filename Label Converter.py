293a470f82a1b79ac0ba350e45cccd7ad8150316
nav17 <cayendo1@gmail.com>
2022-09-16 23:50:40 +0100

    1 file for both os, windows file closing solution

--- Label Converter.py
+++ Label Converter.py
@@ -1,3 +1,4 @@
+# v0.1.2
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import os.path
 import tkinter as tk
@@ -8,12 +9,17 @@ import numpy as np
 from tkinter import messagebox
 from PIL import Image
 import subprocess
+import platform
 
 # gen gui
 root = tk.Tk()
 root.title('Label Converter')
 
-root.geometry("330x350")
+if(platform.system() == "Darwin"):
+    root.geometry("375x345")
+elif(platform.system() == "Windows"):
+    root.geometry("360x340")
+
 root.eval('tk::PlaceWindow . center')
 root.columnconfigure(0)
 
@@ -31,9 +37,12 @@ for i in range(n_columns):
     root.grid_columnconfigure(i,  weight =1)
 
 frame = tk.Frame(root)
-frame.grid(column=0, row=3, padx=20, pady =15, columnspan=2)
+frame.grid(column=0, row=3, padx=20, pady =10, columnspan=2)
 
-lb = tk.Listbox(frame, width="30", height="10")
+if(platform.system() == "Darwin"):
+    lb = tk.Listbox(frame, width="35", height="10")
+elif(platform.system() == "Windows"):
+    lb = tk.Listbox(frame, width="50", height="10")
 lb.grid(column=0,row=0)
 
 class AutoScrollbar(tk.Scrollbar):
@@ -59,9 +68,10 @@ def file_chooser():
     for file in files:
         print("chosen: " + file)
         file_list.append(file)
-        lb.insert("end", file)
+        f = os.path.basename(file)
+        lb.insert("end", f)
     if(file_list!=[]):
-        button3.grid(column=0,row=2, padx=20, pady =0, sticky="W")
+        button3.grid(column=0,row=2, padx=25, pady =0, sticky="W")
 
 def make_label():
     print(file_list)
@@ -69,7 +79,8 @@ def make_label():
     if file_list == []:
         messagebox.showwarning(message="No labels selected")
     for file in file_list:
-        edit=PdfFileReader(open(f'{file}', 'rb'))
+        infile = open(f'{file}', 'rb')
+        edit=PdfFileReader(infile)
         print(file)
         file_dir = os.path.dirname(file)
         file_name = os.path.basename(file)
@@ -123,11 +134,11 @@ def make_label():
             print("saved as new pdf")
 
             # add qr pdf to print
-            qredit=PdfFileReader(open(f'{lacedqr_path}', 'rb'))
-            page = qredit.getPage(0)
+            qr_file = open(f'{lacedqr_path}', 'rb')
+            qr_edit = PdfFileReader(qr_file)
+            page = qr_edit.getPage(0)
             newlabel.addPage(page)
             print("added to print")
-            os.remove(lacedqr_path)
             os.remove(output)
 
             page = edit.getPage(1)
@@ -205,16 +216,21 @@ def make_label():
             print("saved as new pdf")
 
             # add qr pdf to print
-            qredit=PdfFileReader(open(f'{aliasqr_path}', 'rb'))
-            page = qredit.getPage(0)
+            qr_file = open(f'{aliasqr_path}', 'rb')
+            qr_edit=PdfFileReader(qr_file)
+            page = qr_edit.getPage(0)
             newlabel.addPage(page)
             print("added to print")
-            os.remove(aliasqr_path)
             os.remove(output)
     
-    new_label_path = os.path.join(file_dir, 'newlabel.pdf')
-    Output = open(f'{new_label_path}', 'wb')
-    newlabel.write(Output)
+        new_label_path = os.path.join(file_dir, 'newlabel.pdf')
+        Output = open(f'{new_label_path}', 'wb')
+        newlabel.write(Output)
+        qr_file.close()
+        if file_name.endswith('- Shipping Label.pdf'):
+            os.remove(lacedqr_path)
+        else:
+            os.remove(aliasqr_path)
     Output.close()
     if(switch.get() == 1):
         for file in file_list:
@@ -222,7 +238,10 @@ def make_label():
         file_list.clear()
         lb.delete(0,'end')
     messagebox.showinfo(title="Done", message="Labels ready to print!")
-    subprocess.run(['open', new_label_path], check=True)
+    if(platform.system() == "Darwin"):
+        subprocess.run(['open', new_label_path], check=True)
+    elif(platform.system() == "Windows"):
+        os.startfile(new_label_path)
     print("done")
 
 def clear_list():
@@ -230,35 +249,26 @@ def clear_list():
     lb.delete(0,'end')
     button3.grid_forget()
 
-def delete_labels():
-    for file in file_list:
-        os.remove(file)
-    file_list.clear()
-    lb.delete(0,'end')
-
 switch = tk.IntVar()
 
 label1 = tk.Label(root, text='Add Laced and/or Alias labels here:')
-label1.grid(column=0,row=0, padx=20, pady =5, columnspan=2, sticky="W")
+label1.grid(column=0,row=0, padx=25, pady =5, columnspan=2, sticky="W")
 
 button2 = tk.Button(text="Choose Files", command=file_chooser)
-button2.grid(column=0,row=1, padx=20, pady=0, sticky="W")
+button2.grid(column=0,row=1, padx=25, pady=0, sticky="W")
 
 button1 = tk.Button(text="Submit Labels", command=make_label)
 button1.grid(column=0,row=4, pady =0, columnspan=2)
 
 button3 = tk.Button(text="Clear List", command=clear_list)
-button3.grid(column=1,row=2, padx=20, pady =0, sticky="E")
-
-#button4 = tk.Button(text="Delete Labels", command=delete_labels)
-#button4.grid(column=1,row=3, pady =0)
+button3.grid(column=1,row=2, padx=25, pady =0, sticky="E")
 
 checkbox1 = tk.Checkbutton(root, text="auto-delete labels", variable=switch, onvalue=1, offvalue=0)
-checkbox1.grid(column=1, row=1, padx=20, pady =0, sticky="E")
+checkbox1.grid(column=1, row=1, padx=25, pady =0, sticky="E")
 
 if(file_list == []):
     button3.grid_forget()
-    checkbox1.grid(column=1, row=1, padx=20, pady =0, sticky="E")
+    checkbox1.grid(column=1, row=1, padx=25, pady =0, sticky="E")
 
 label2 = tk.Label(root)
 label2.grid(column=0, row=5, columnspan=2)
